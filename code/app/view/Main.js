914deb69cb5457d3aaf947da875194de3d05d602
Paul Roth <paulroth3d@gmail.com>
2012-03-21 02:31:32 -0500

    UPDATE: add in the required hello world example

--- code/app/view/Main.js
+++ code/app/view/Main.js
@@ -23,23 +23,6 @@ Ext.define("senchaTalk.view.Main", {
                     '<img src="http://staging.sencha.com/img/sencha.png" />',
 					"<div align='center'>Whassap</p>"
                 ].join("")
-            },
-            {
-                title: 'Get Started',
-                iconCls: 'action',
-                
-                items: [
-                    {
-                        docked: 'top',
-                        xtype: 'titlebar',
-                        title: 'Getting Started'
-                    },
-                    {
-                        xtype: 'video',
-                        url: 'http://av.vimeo.com/64284/137/87347327.mp4?token=1330978144_f9b698fea38cd408d52a2393240c896c',
-                        posterUrl: 'http://b.vimeocdn.com/ts/261/062/261062119_640.jpg'
-                    }
-                ]
             }
         ]
     }
