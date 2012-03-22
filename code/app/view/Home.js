b6a648424d484d830a4605d7aa16761f1b78df6b
Paul Roth <paulroth3d@gmail.com>
2012-03-21 22:25:28 -0500

    UPDATE: instead of showing everything in an html container, they can be separated out through an image component

--- code/app/view/Home.js
+++ code/app/view/Home.js
@@ -10,14 +10,23 @@ Ext.define( 'senchaTalk.view.Home', {
 	
 	config: {
 		fullscreen: true,
-		html: [
-			'<img src="http://staging.sencha.com/img/sencha.png" />',
-			'<h1>Welcome to Sencha Touch</h1>',
-			"<p>You're creating the Getting Started app. This demonstrates how ",
-			"to use tabs, lists and forms to create a simple app</p>",
-			'<h2>Sencha Touch (2.0.0)</h2>'
-		].join(""),
-		styleHtmlContent: true,
-		scrollable: true
+		scrollable: true,
+		layout: 'vbox',
+		
+		items: [{
+			xtype: 'image',
+			src: 'http://staging.sencha.com/img/sencha.png',
+			width: 210,
+			height: 291
+		},{
+			xtype: 'panel',
+			styleHtmlContent: true,
+			html: [
+				'<h1>Welcome to Sencha Touch</h1>',
+				"<p>You're creating the Getting Started app. This demonstrates how ",
+				"to use tabs, lists and forms to create a simple app</p>",
+				'<h2>Sencha Touch (2.0.0)</h2>'
+			].join("")
+		}]
 	}
 });
