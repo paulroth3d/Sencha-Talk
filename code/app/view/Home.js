6ca68bfbcb4d586253ed010862ea5eb9048c324d
Paul Roth <paulroth3d@gmail.com>
2012-03-21 22:27:42 -0500

    REVERT: for now, lets keep the home image in html for simplicity.

--- code/app/view/Home.js
+++ code/app/view/Home.js
@@ -13,20 +13,13 @@ Ext.define( 'senchaTalk.view.Home', {
 		scrollable: true,
 		layout: 'vbox',
 		
-		items: [{
-			xtype: 'image',
-			src: 'http://staging.sencha.com/img/sencha.png',
-			width: 210,
-			height: 291
-		},{
-			xtype: 'panel',
-			styleHtmlContent: true,
-			html: [
-				'<h1>Welcome to Sencha Touch</h1>',
-				"<p>You're creating the Getting Started app. This demonstrates how ",
-				"to use tabs, lists and forms to create a simple app</p>",
-				'<h2>Sencha Touch (2.0.0)</h2>'
-			].join("")
-		}]
+		styleHtmlContent: true,
+		html: [
+			'<img src="http://staging.sencha.com/img/sencha.png" />',
+			'<h1>Welcome to Sencha Touch</h1>',
+			"<p>You're creating the Getting Started app. This demonstrates how ",
+			"to use tabs, lists and forms to create a simple app</p>",
+			'<h2>Sencha Touch (2.0.0)</h2>'
+		].join("")
 	}
 });
